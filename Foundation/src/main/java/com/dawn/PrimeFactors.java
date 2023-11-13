29c659599960cf828bdc48a3fc34bfd56f3c2adb
Nithin Prasad <nithin2889@gmail.com>
2023-11-13 17:53:56 +0530

    prime factors of a number

---
+++ Foundation/src/main/java/com/dawn/PrimeFactors.java
@@ -0,0 +1,27 @@
+package com.dawn;
+
+import java.util.Scanner;
+
+public class PrimeFactors {
+
+  public static void main(String[] args) {
+    System.out.print("Please enter a number: ");
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    System.out.println("The prime factors of number " + n + " are: ");
+    primeFactors(n);
+  }
+
+  private static void primeFactors(int n) {
+    for (int dsr = 2; dsr <= n; dsr++) {
+      while (n % dsr == 0) {
+        n = n / dsr;
+        System.out.print(dsr + " ");
+      }
+    }
+
+    if (n != 1) {
+      System.out.print(n);
+    }
+  }
+}

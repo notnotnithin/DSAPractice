11fb3143106fda4131c1c468b059b097af64c187
Nithin Prasad <nithin2889@gmail.com>
2023-12-15 11:16:56 +0530

    Solid half diamond pattern

--- Patterns/src/main/java/com/dawn/Pattern21.java
+++ Patterns/src/main/java/com/dawn/Pattern21.java
@@ -1,2 +1,38 @@
-package com.dawn;public class Pattern21 {
+package com.dawn;
+
+import java.util.Scanner;
+
+public class Pattern21 {
+
+  public static void main(String[] args) {
+    System.out.println("Solid half diamond pattern:");
+    Scanner scn = new Scanner(System.in);
+    System.out.println("Input an integer: ");
+    int N = scn.nextInt();
+
+    /**
+     *
+     * *
+     * **
+     * ***
+     * ****
+     * ***
+     * **
+     * *
+     *
+     */
+
+    for (int row = 1; row <= N; row++) {
+      for (int col = 1; col <= row; col++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    for (int row = 1; row < N; row++) {
+      for (int col = 1; col <= N - row; col++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
 }

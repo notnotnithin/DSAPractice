507f386655831678c4c41b8d1ea17e2b25cc6185
Nithin Prasad <nithin2889@gmail.com>
2023-12-15 11:15:36 +0530

    Numeric palindrome equilateral pyramid pattern

--- Patterns/src/main/java/com/dawn/Pattern19.java
+++ Patterns/src/main/java/com/dawn/Pattern19.java
@@ -1,2 +1,45 @@
-package com.dawn;public class Pattern19 {
+package com.dawn;
+
+import java.util.Scanner;
+
+public class Pattern19 {
+
+  public static void main(String[] args) {
+    System.out.println("Numeric palindrome equilateral pyramid pattern:");
+    Scanner scn = new Scanner(System.in);
+    System.out.println("Input an integer: ");
+    int N = scn.nextInt();
+
+    /**
+     *
+     *
+     *  	 1
+     *    121
+     *   12321
+     *  1234321
+     * 123454321
+     *
+     *
+     */
+
+    int k = 1;
+    for (int row = 1; row <= N; row++) {
+      for (int col = 1; col <= N - row; col++) {
+        System.out.print(" ");
+      }
+
+      k = 1;
+      for (int col = 1; col <= row; col++) {
+        System.out.print(k);
+        k++;
+      }
+
+      int r = 1;
+      for (int col = 1; col < row; col++) {
+        System.out.print(r);
+        r++;
+      }
+      System.out.println();
+    }
+  }
 }

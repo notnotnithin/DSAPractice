8505f8407b804f2f5f4c16872086e351f4978a8f
Nithin Prasad <nithin2889@gmail.com>
2023-12-15 11:16:30 +0530

    Stars and numeric solid triangle pattern

--- Patterns/src/main/java/com/dawn/Pattern20.java
+++ Patterns/src/main/java/com/dawn/Pattern20.java
@@ -1,2 +1,41 @@
-package com.dawn;public class Pattern20 {
+package com.dawn;
+
+import java.util.Scanner;
+
+public class Pattern20 {
+
+  public static void main(String[] args) {
+    System.out.println("Stars and numeric solid triangle pattern:");
+    Scanner scn = new Scanner(System.in);
+    System.out.println("Input an integer: ");
+    int N = scn.nextInt();
+
+    /**
+     *
+     * ********1********
+     * *******2*2*******
+     * ******3*3*3******
+     * *****4*4*4*4*****
+     * ****5*5*5*5*5****
+     */
+
+    for (int row = 0; row < N; row++) {
+      for (int col = 0; col < 2 * N - 2 - row; col++) {
+        System.out.print("*");
+      }
+
+      for (int col = 0; col < 2 * row + 1; col++) {
+        if (col % 2 == 0) {
+          System.out.print(row + 1);
+        } else {
+          System.out.print("*");
+        }
+      }
+
+      for (int col = 0; col < 2 * N - 2 - row; col++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
 }

357ea0eb556c1b28096a35aeeab926d6141d4d62
Nithin <apple@Nithins-MacBook-Pro.local>
2025-10-11 20:10:16 +0530

    Binary to decimal conversion

--- Foundation/src/main/java/com/dawn/BinaryNumberSystem.java
+++ Foundation/src/main/java/com/dawn/BinaryNumberSystem.java
@@ -2,33 +2,48 @@ package com.dawn;
 
 public class BinaryNumberSystem {
 
-    static void main(String[] args) {
-        String num = IO.readln("Enter a positive number: ");
-        IO.println(decimalToBinaryUsingDivisionMethod(Integer.parseInt(num)));
-        IO.println(decimalToBinaryUsingBitwiseMethod(Integer.parseInt(num)));
-    }
+  static void main(String[] args) {
+    String num1 = IO.readln("Enter a positive number: ");
+    IO.println(decimalToBinaryUsingDivisionMethod(Integer.parseInt(num1)));
+    IO.println(decimalToBinaryUsingBitwiseMethod(Integer.parseInt(num1)));
 
-    private static int decimalToBinaryUsingDivisionMethod(int num) {
-        int binaryNum = 0;
-        int i = 0;
-        while (num > 0) {
-            int bit = num % 2;
-            binaryNum = (int) (bit * Math.pow(10, i) + binaryNum);
-            num = num / 2;
-            i = i + 1;
-        }
-        return binaryNum;
-    }
+    String num2 = IO.readln("Enter a positive number: ");
+    IO.println(binaryToDecimalUsingDivisionMethod(Integer.parseInt(num2)));
+  }
 
-    private static int decimalToBinaryUsingBitwiseMethod(int num) {
-        int binaryNum = 0;
-        int i = 0;
-        while (num > 0) {
-            int bit = num & 1;
-            binaryNum = (int) (bit * Math.pow(10, i) + binaryNum);
-            num = num >> 1;
-            i = i + 1;
-        }
-        return binaryNum;
+  private static int decimalToBinaryUsingDivisionMethod(int num) {
+    int binaryNum = 0;
+    int i = 0;
+    while (num > 0) {
+      int bit = num % 2;
+      binaryNum = (int) (bit * Math.pow(10, i) + binaryNum);
+      num = num / 2;
+      i = i + 1;
     }
+    return binaryNum;
+  }
+
+  private static int decimalToBinaryUsingBitwiseMethod(int num) {
+    int binaryNum = 0;
+    int i = 0;
+    while (num > 0) {
+      int bit = num & 1;
+      binaryNum = (int) (bit * Math.pow(10, i) + binaryNum);
+      num = num >> 1;
+      i = i + 1;
+    }
+    return binaryNum;
+  }
+
+  private static int binaryToDecimalUsingDivisionMethod(int num) {
+    int decimal = 0;
+    int i = 0;
+    while (num > 0) {
+      int bit = num % 10;
+      decimal = (int) (bit * Math.pow(2, i) + decimal);
+      num = num / 10;
+      i = i + 1;
+    }
+    return decimal;
+  }
 }

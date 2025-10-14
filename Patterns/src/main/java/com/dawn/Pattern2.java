5e4006300f509152f27d54cf9ef10fc83d1d1e2d
Nithin <apple@Nithins-MacBook-Pro.local>
2025-10-14 21:14:18 +0530

    Pyramid pattern questions

--- Patterns/src/main/java/com/dawn/Pattern2.java
+++ Patterns/src/main/java/com/dawn/Pattern2.java
@@ -2,15 +2,42 @@ package com.dawn;
 
 public class Pattern2 {
 
-  public static void main(String[] args) {
-    System.out.println("Inverted Half Pyramid Pattern!");
-    int n = 5;
+    static void main(String[] args) {
+        int n = Integer.parseInt(IO.readln("Enter a number: "));
+        System.out.println("Inverted Half Pyramid Pattern!");
+        invertedHalfPyramid(n);
 
-    for (int row = 0; row < n; row++) {
-      for (int col = 0; col < n - row; col++) {
-        System.out.print("* ");
-      }
-      System.out.println();
+        System.out.println("Numerical Half Pyramid Pattern!");
+        numericHalfPyramid(n);
+
+        System.out.println("Inverted Numeric Half Pyramid Pattern!");
+        invertedNumericHalfPyramid(n);
     }
-  }
+
+    private static void invertedHalfPyramid(int n) {
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n - row; col++) {
+                IO.print("* ");
+            }
+            IO.println();
+        }
+    }
+
+    private static void numericHalfPyramid(int n) {
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < row + 1; col++) {
+                IO.print(col + 1);
+            }
+            IO.println();
+        }
+    }
+
+    private static void invertedNumericHalfPyramid(int n) {
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col <= n - row - 1; col++) {
+                IO.print(col + 1);
+            }
+            IO.println();
+        }
+    }
 }

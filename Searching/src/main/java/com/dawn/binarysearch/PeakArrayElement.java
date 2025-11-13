4dd8a5d84a731dbf857324900c033b1ddf587519
Nithin <apple@Nithins-MacBook-Pro.local>
2025-11-13 20:18:53 +0530

    Peak element in an array using binary search

---
+++ Searching/src/main/java/com/dawn/binarysearch/PeakArrayElement.java
@@ -0,0 +1,26 @@
+package com.dawn.binarysearch;
+
+public class PeakArrayElement {
+
+    static void main() {
+        int[] a = {10,20,50,40,30};
+        int peakElement = PeakArrayElement.searchPeakElement(a);
+        IO.println("Peak element in the array is at index: " + peakElement);
+    }
+
+    public static int searchPeakElement(int[] arr) {
+        int s = 0;
+        int e = arr.length - 1;
+        int m;
+
+        while (s < e) {
+            m = s + (e - s) / 2;
+            if (arr[m] < arr[m + 1]) {
+                s = m + 1;
+            } else {
+                e = m;
+            }
+        }
+        return s;
+    }
+}
